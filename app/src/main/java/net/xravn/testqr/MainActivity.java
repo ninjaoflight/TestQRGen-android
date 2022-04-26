1aeb44b109adad8c2ba68d8ba205b3f8da6c4971
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-26 17:34:09 -0600

    commit inicial

---
+++ app/src/main/java/net/xravn/testqr/MainActivity.java
@@ -0,0 +1,20 @@
+package net.xravn.testqr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/net/xravn/testqr/QRGenerator.java
@@ -0,0 +1,5 @@
+package net.xravn.testqr;
+
+public class QRGenerator {
+
+}
