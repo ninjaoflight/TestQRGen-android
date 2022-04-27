7e5cba7714f887ffff887dcdb3d4a46b85f87475
Jonathan Guzmán <jonguz5@hotmail.com>
2022-04-26 18:22:11 -0600

    generate QR code - bind elements to the activity - create helper QRGenerator class (create&put)

--- app/src/main/java/net/xravn/testqr/MainActivity.java
+++ app/src/main/java/net/xravn/testqr/MainActivity.java
@@ -6,15 +6,32 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtContent;
+    ImageView imgQr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        txtContent = findViewById(R.id.txt_qr_input);
+        imgQr = findViewById(R.id.imageView_qr_image);
+        onWindowFocusChanged(true);
+    }
+
+
+    public void onBtnCreateQrClicked(View v) {
+        QRGenerator.setQR(txtContent.getText().toString(), imgQr);
     }
 }
--- app/src/main/java/net/xravn/testqr/QRGenerator.java
+++ app/src/main/java/net/xravn/testqr/QRGenerator.java
@@ -1,5 +1,18 @@
 package net.xravn.testqr;
 
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import net.glxn.qrgen.android.QRCode;
+
+
 public class QRGenerator {
 
+    public static void setQR(String text, ImageView imageView) {
+        imageView.setImageBitmap(generateQR(text, imageView.getWidth(), imageView.getHeight()));
+    }
+
+    public static Bitmap generateQR(String text, int width, int height) {
+        return QRCode.from(text).withSize(width, height).bitmap();
+    }
 }
